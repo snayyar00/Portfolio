db70839b552780c82b97990b1a549b929c792826
snayyar00 <39316104+snayyar00@users.noreply.github.com>
2019-10-30 04:06:14 -0400

    Add files via upload

--- Portfolio-Sidharth/js/front.js
+++ Portfolio-Sidharth/js/front.js
@@ -34,15 +34,15 @@ $(function () {
         e.preventDefault();
     });
 
-    
+
 
     var stylesheet = $('link#theme-stylesheet');
     $("<link id='new-stylesheet' rel='stylesheet'>").insertAfter(stylesheet);
     var alternateColour = $('link#new-stylesheet');
 
-    if ($.cookie("theme_csspath")) {
-        alternateColour.attr("href", $.cookie("theme_csspath"));
-    }
+    // if ($.cookie("theme_csspath")) {
+    //     alternateColour.attr("href", $.cookie("theme_csspath"));
+    // }
 
     $("#colour").change(function () {
 
@@ -51,11 +51,6 @@ $(function () {
             var theme_csspath = 'css/style.' + $(this).val() + '.css';
 
             alternateColour.attr("href", theme_csspath);
-
-            $.cookie("theme_csspath", theme_csspath, {
-                expires: 365,
-                path: document.URL.substr(0, document.URL.lastIndexOf('/'))
-            });
 
         }
 
